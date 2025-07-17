cb606e67d0f17e1b70afe9fcd19d99e484452aff
Edgecoordinates <Edgecoordinates@gmail.com>
2025-07-17 16:01:20 +0800

    feat(i18n): enhance react-i18next for translation support and update settings
    - Added i18n-ally configuration for react-i18next in VSCode settings.
    - Implemented translation functionality in ReviewByBookPage and ReviewPage components.
    - Removed DYNAMIC_THEME_README.md as it is no longer needed.
    - Updated tsconfig.json to include type definitions for i18next.

--- package/app/src/page/Review/ReviewByBookPage.tsx
+++ package/app/src/page/Review/ReviewByBookPage.tsx
@@ -1,14 +1,17 @@
 import { useParams } from 'wouter'
 
 import { ReviewList } from '@/component/Review/ReviewList';
+import { useTranslation } from 'react-i18next';
 
 export function ReviewByBookPage() {
     const params = useParams();
     const bookId = params[0];
+    const { t } = useTranslation();
     
     return (
         <div>
             <ReviewList.Show reviews={[]} isReplyModalOpen={false} currentReplyId={null} onReply={() => {}} onCloseReplyModal={() => {}} />
+            {t("pages.book_list_edit_page")}
         </div>
     )
 }
