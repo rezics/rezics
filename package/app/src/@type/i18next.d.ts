b0a042cc2d2af29aa2c6de22102eb3191e6fdb3e
Edgecoordinates <Edgecoordinates@gmail.com>
2025-07-17 16:01:20 +0800

    feat(i18n): enhance react-i18next for translation support and update settings
    - Added i18n-ally configuration for react-i18next in VSCode settings.
    - Implemented translation functionality in ReviewByBookPage and ReviewPage components.
    - Removed DYNAMIC_THEME_README.md as it is no longer needed.
    - Updated tsconfig.json to include type definitions for i18next.

---
+++ package/app/src/@type/i18next.d.ts
@@ -0,0 +1,11 @@
+import 'i18next'
+import type enUS from '../locale/en-US'
+
+declare module 'i18next' {
+  interface CustomTypeOptions {
+    defaultNS: 'translation'
+    resources: {
+      translation: typeof enUS
+    }
+  }
+}
--- package/app/src/page/Review/ReviewPage.tsx
+++ package/app/src/page/Review/ReviewPage.tsx
@@ -1,5 +1,6 @@
- ;
+import { useTranslation } from "react-i18next";
 
 export function LongReviewPage() {
-    return <div>{t("pages->long_review_page")}</div>;
+    const { t } = useTranslation();
+    return <div>{t("pages.long_review_page")}</div>;
 }
