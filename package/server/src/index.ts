34c31639def81796b7b96a2963172a94e83e7761
Jeb Feng <ranzeplay@outlook.com>
2025-07-23 15:56:50 +0800

    chore: update pnpm workspace configuration
    - Added '@swc/core' to onlyBuiltDependencies
    - Reformatted the pnpm-workspace.yaml for better readability

--- package/server/src/index.ts
+++ package/server/src/index.ts
@@ -1,9 +1,12 @@
 import { createClient } from "gel";
-import e from "../database/ts/index.js";
+import e from "../database/ts/index";
 import { contract, ts_fastify } from "contract";
+import Fastify from "fastify";
 
 const client = createClient();
 
-contract.tag.get();
+const app = Fastify({ logger: true });
 
-const server = ts_fastify.initServer();
+const host = process.env['WEBSERVER_HOST'] || '0.0.0.0';
+const port = process.env['WEBSERVER_PORT'] ? parseInt(process.env['WEBSERVER_PORT']) : 3000;
+await app.listen({ host, port });
