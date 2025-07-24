4f0e7ae63a08e00772e8dc0ba5ae01e32aecd01f
NMNM.CC <huangyinhaow@outlook.com>
2025-07-24 20:29:41 +0800

    refactor: Enable esModuleInterop in tsconfig.json for improved module compatibility

--- package/server/src/index.ts
+++ package/server/src/index.ts
@@ -1,12 +1,3 @@
-import { createClient } from "gel";
-import e from "../database/ts/index";
-import { contract, ts_fastify } from "contract";
-import Fastify from "fastify";
+import c from "contract";
 
-const client = createClient();
-
-const app = Fastify({ logger: true });
-
-const host = process.env['WEBSERVER_HOST'] || '0.0.0.0';
-const port = process.env['WEBSERVER_PORT'] ? parseInt(process.env['WEBSERVER_PORT']) : 3000;
-await app.listen({ host, port });
+console.log(c);
