3307bad49b85febf85525d9b21949206b32343fd
Jeb Feng <ranzeplay@outlook.com>
2025-07-24 23:34:48 +0800

    feat: server homepage route demo

--- package/server/src/index.ts
+++ package/server/src/index.ts
@@ -1,3 +1,28 @@
+import { initServer } from "@ts-rest/fastify";
 import c from "contract";
+import fastify from "fastify";
 
-console.log(c);
+const server = initServer();
+const router = server.router(c.Homepage, {
+	get: async () => {
+		const homePageData = {
+			id: crypto.randomUUID(),
+			content: "Hello, world!"
+		};
+
+		return {
+			status: 200,
+			body: homePageData,
+		};
+	}
+});
+
+const app = fastify({ logger: true });
+app.register(server.plugin(router));
+
+const host = process.env['WEBSERVER_HOST'] || '0.0.0.0';
+const port = process.env['WEBSERVER_PORT'] ? parseInt(process.env['WEBSERVER_PORT']) : 3000;
+
+console.log(`Starting server on ${host}:${port}`);
+
+await app.listen({ host, port });
