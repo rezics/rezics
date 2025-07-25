9636d63ae6c514f7284edf0b3e655ac52d201ae1
Edgecoordinates <Edgecoordinates@gmail.com>
2025-07-25 22:36:29 +0800

    feat: add Husky to ensure proper dev execution

--- package/server/src/index.ts
+++ package/server/src/index.ts
@@ -10,23 +10,23 @@ import Homepage from "./router/Homepage";
 import Tag from "./router/Tag";
 
 const main = async () => {
-    const { host, port } = Config.parse(process.env);
-    const f = Fastify({ logger: true });
+    // const { host, port } = Config.parse(process.env);
+    // const f = Fastify({ logger: true });
 
-    const r = s.router(c, {
-        Auth,
-        Book,
-        Chapter,
-        Homepage,
-        Tag,
-    });
+    // const r = s.router(c, {
+    //     Auth,
+    //     Book,
+    //     Chapter,
+    //     Homepage,
+    //     Tag,
+    // });
 
-    f.register(s.plugin(r));
+    // f.register(s.plugin(r));
 
-    f.listen({
-        host,
-        port,
-    });
+    // f.listen({
+    //     host,
+    //     port,
+    // });
 };
 
 main().catch((err) => {
