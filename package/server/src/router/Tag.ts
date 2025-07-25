68de1badc06e4fd619fb408ce1ff64f94518fc2a
NMNM.CC <huangyinhaow@outlook.com>
2025-07-25 17:03:12 +0800

    refactor: redefine the database schema

--- package/server/src/router/Tag.ts
+++ package/server/src/router/Tag.ts
@@ -2,4 +2,15 @@ import s from "./s";
 import c from "contract";
 import d from "database";
 
-export default s.router(c.Tag, {});
+export default s.router(c.Tag, {
+    create: async ({ body }) => {
+        d.insert(d.CustomTag, {
+            name: body.name,
+            owner: d.select(d.User.i),
+        });
+
+        return {
+            200: {},
+        };
+    },
+});
