c10e357edd3b750ba71b7e402efd31af9f9cb8ea
Edge-coordinates <Edgecoordinates@gmail.com>
2026-04-21 13:47:59 +0800

    fix: fix cosmos render error

--- package/app/src/cosmos.decorator.tsx
+++ package/app/src/cosmos.decorator.tsx
@@ -2,7 +2,9 @@ import { ThemeProvider } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import { StyledEngineProvider } from "@mui/material/styles";
 import { getTheme } from "@rezics/ui";
+import { QueryClientProvider } from "@tanstack/react-query";
 import type React from "react";
+import { qc } from "./app/providers/reactQueryUtil";
 
 export default function GlobalDecorator({
   children,
@@ -12,11 +14,13 @@ export default function GlobalDecorator({
   const theme = getTheme("light");
 
   return (
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        {children}
-      </ThemeProvider>
-    </StyledEngineProvider>
+    <QueryClientProvider client={qc}>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </QueryClientProvider>
   );
 }
