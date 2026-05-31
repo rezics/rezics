f3d4185f65a1b2c3c8cde3d22cf579547c46ce74
Edge-coordinates <Edgecoordinates@gmail.com>
2026-06-01 03:44:45 +0800

    refactor(api): remove translation group client

--- package/api/src/translation-group/translation-group.hooks.ts
+++
@@ -1,6 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { translationGroupSiblingsQuery } from "./translation-group.queries";
-
-export function useTranslationGroupSiblings(unitId: string | null | undefined) {
-  return useQuery(translationGroupSiblingsQuery(unitId));
-}
