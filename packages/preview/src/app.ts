1b7d940e8e20c2d53ab6e21ce4c790fc2e3cb7ca
NMNM.CC <huangyinhaow@outlook.com>
2026-06-25 04:57:17 +0800

    chore: remove preview compatibility package

--- packages/preview/src/app.ts
+++
@@ -1,5 +0,0 @@
-export {
-  createPreviewApp,
-  type CreatePreviewAppOptions,
-  type PreviewApp,
-} from "@rezics/backend/preview/app";
--- packages/preview/src/book/book.api.ts
+++
@@ -1,1 +0,0 @@
-export { bookApi } from "@rezics/backend/preview/book/book.api";
