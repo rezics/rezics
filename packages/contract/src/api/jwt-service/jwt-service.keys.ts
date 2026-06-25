1b5123a3794b4a7bda02cf03a4ae8c27f65489aa
NMNM.CC <huangyinhaow@outlook.com>
2026-06-25 10:07:18 +0800

    refactor: remove unused contract api wrappers

--- packages/contract/src/api/jwt-service/jwt-service.keys.ts
+++
@@ -1,8 +0,0 @@
-export const jwtServiceKeys = {
-  all: () => ["jwtServices"] as const,
-  lists: () => [...jwtServiceKeys.all(), "list"] as const,
-  list: () => [...jwtServiceKeys.lists()] as const,
-  details: () => [...jwtServiceKeys.all(), "detail"] as const,
-  detail: (serviceKey: string) =>
-    [...jwtServiceKeys.details(), serviceKey] as const,
-} as const;
